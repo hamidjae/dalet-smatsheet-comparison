401daca6a39b66ad15eebeb0b8645531322abb55
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-02-06 15:33:49 -0700

    Added episode view, extra view

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ CSV_FILE = Path("form.csv")
 #Establishing the TTK skeleton
 root = Tk()
 root.title("Dalet Series/Season Form to CSV")
+#VERY IMPORTANT: Establish a notebook BEFORE adding the tabs.. otherwise it does not work
 formFiller = ttk.Notebook(root)
+
+#Adding tabs to the notebook
 seriesView = ttk.Frame(root, padding=15)
 seasonView = ttk.Frame(root, padding=15)
+episodeView = ttk.Frame(root, padding=15)
+extraView = ttk.Frame(root, padding=15)
 
 formFiller.add(seriesView, text="Series Form")
 formFiller.add(seasonView, text="Season Form")
+formFiller.add(episodeView, text="Episode Form")
+formFiller.add(extraView, text="Extra Form")
 formFiller.pack(expand=True, fill="both")
-
-for i in range(8):
-    seriesView.rowconfigure(i, weight=1)
-seriesView.columnconfigure(1, weight=1)
 
 #Series Form
 ttk.Label(seriesView, text="Title").grid(column=0, row=0, sticky="w")
@@ -58,8 +61,12 @@ ttk.Label(seriesView, text="SEO Keywords").grid(column=0, row=8, sticky="w")
 SeriesSEO_text = Text(seriesView, width=60, height=2, wrap="word")
 SeriesSEO_text.grid(column=1, row=8)
 
+ttk.Label(seriesView, text="Expected Duration").grid(column=0, row=9, sticky="w")
+seriesTitle_text = Text(seriesView, height=2, width=60)
+seriesTitle_text.grid(column=1, row=9, sticky="nsew")
+
 #Season Form
-ttk.Label(seasonView, text="Title").grid(column=0, row=0, sticky="w")
+ttk.Label(seasonView, text="Title/Number").grid(column=0, row=0, sticky="w")
 seasonTitle_text = Text(seasonView, height=2, width=60)
 seasonTitle_text.grid(column=1, row=0, sticky="nsew")
 
@@ -87,5 +94,82 @@ ttk.Label(seasonView, text="SEO Keywords").grid(column=0, row=6, sticky="w")
 SeasonSEO_text = Text(seasonView, width=60, height=2, wrap="word")
 SeasonSEO_text.grid(column=1, row=6)
 
+#Episode Form
+ttk.Label(episodeView, text="Title").grid(column=0, row=0, sticky="w")
+episodeTitle_text = Text(episodeView, height=2, width=60)
+episodeTitle_text.grid(column=1, row=0, sticky="nsew")
+
+ttk.Label(episodeView, text="Short Title (25 chars)").grid(column=0, row=1, sticky="w")
+episodeShortTitle_text = Text(episodeView, height=2, width=60, wrap="word")
+episodeShortTitle_text.grid(column=1, row=1)
+
+ttk.Label(episodeView, text="Sort Title").grid(column=0, row=2, sticky="w")
+episodeSortTitle_text = Text(episodeView, height = 2, width=60)
+episodeSortTitle_text.grid(column = 1, row = 2)
+
+ttk.Label(episodeView, text="Long (1000 chars)").grid(column=0, row=3, sticky="w")
+episodeLong_text = Text(episodeView, width=60, height=11, wrap="word")
+episodeLong_text.grid(column=1, row=3)
+
+ttk.Label(episodeView, text="Medium (250 chars)").grid(column=0, row=4, sticky="w")
+episodeMedium_text = Text(episodeView, width=60, height=5, wrap="word")
+episodeMedium_text.grid(column=1, row=4)
+
+ttk.Label(episodeView, text="Short (99 chars)").grid(column=0, row=5, sticky="w")
+episodeShort_text = Text(episodeView, width=60, height=4, wrap="word")
+episodeShort_text.grid(column=1, row=5)
+
+ttk.Label(episodeView, text="XShort (60 chars)").grid(column=0, row=6, sticky="w")
+episodeXShort_text = Text(episodeView, width=60, height=3, wrap="word")
+episodeXShort_text.grid(column=1, row=6)
+
+ttk.Label(episodeView, text="Tiny (40 chars)").grid(column=0, row=7, sticky="w")
+episodeTiny_text = Text(episodeView, width=60, height=2, wrap="word")
+episodeTiny_text.grid(column=1, row=7)
+
+ttk.Label(episodeView, text="SEO Keywords").grid(column=0, row=8, sticky="w")
+episodeSEO_text = Text(episodeView, width=60, height=2, wrap="word")
+episodeSEO_text.grid(column=1, row=8)
+
+ttk.Label(episodeView, text="Expected Duration").grid(column=0, row=9, sticky="w")
+episodeTitle_text = Text(episodeView, height=2, width=60)
+episodeTitle_text.grid(column=1, row=9, sticky="nsew")
+
+#Extra Form
+ttk.Label(extraView, text="Title").grid(column=0, row=0, sticky="w")
+episodeTitle_text = Text(extraView, height=2, width=60)
+episodeTitle_text.grid(column=1, row=0, sticky="nsew")
+
+ttk.Label(extraView, text="Short Title (25 chars)").grid(column=0, row=1, sticky="w")
+episodeShortTitle_text = Text(extraView, height=2, width=60, wrap="word")
+episodeShortTitle_text.grid(column=1, row=1)
+
+ttk.Label(extraView, text="Sort Title").grid(column=0, row=2, sticky="w")
+episodeSortTitle_text = Text(extraView, height = 2, width=60)
+episodeSortTitle_text.grid(column = 1, row = 2)
+
+ttk.Label(extraView, text="Long (1000 chars)").grid(column=0, row=3, sticky="w")
+episodeLong_text = Text(extraView, width=60, height=11, wrap="word")
+episodeLong_text.grid(column=1, row=3)
+
+ttk.Label(extraView, text="Medium (250 chars)").grid(column=0, row=4, sticky="w")
+episodeMedium_text = Text(extraView, width=60, height=5, wrap="word")
+episodeMedium_text.grid(column=1, row=4)
+
+ttk.Label(extraView, text="Short (99 chars)").grid(column=0, row=5, sticky="w")
+episodeShort_text = Text(extraView, width=60, height=4, wrap="word")
+episodeShort_text.grid(column=1, row=5)
+
+ttk.Label(extraView, text="XShort (60 chars)").grid(column=0, row=6, sticky="w")
+episodeXShort_text = Text(extraView, width=60, height=3, wrap="word")
+episodeXShort_text.grid(column=1, row=6)
+
+ttk.Label(extraView, text="Tiny (40 chars)").grid(column=0, row=7, sticky="w")
+episodeTiny_text = Text(extraView, width=60, height=2, wrap="word")
+episodeTiny_text.grid(column=1, row=7)
+
+ttk.Label(extraView, text="Expected Duration").grid(column=0, row=8, sticky="w")
+extraTitle_text = Text(extraView, height=2, width=60)
+extraTitle_text.grid(column=1, row=8, sticky="nsew")
 
 root.mainloop()
