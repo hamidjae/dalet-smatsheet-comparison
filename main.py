0c367995959b1d155a3bf4f06c0451f6c8e1d1a0
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-03-03 13:32:43 -0700

    Added CSV saving to Series Form.

--- main.py
+++ main.py
@@ -6,6 +6,54 @@ import csv
 #Establishing the name of the file
 CSV_FILE = Path("form.csv")
 
+def store_entry(text):
+    return text.get("1.0", "end-1c").strip()
+
+def split_seo_keywords(text):
+    keywords = []
+    words = text.split(";")
+    for word in words:
+        cleaned_word = word.strip()
+        if cleaned_word:
+            keywords.append(cleaned_word)
+    return keywords
+
+def store_series_form():
+    row={
+        "form_type": "series",
+        "title": store_entry(SeriesTitle_Titletext),
+        "short_title": store_entry(SeriesShortTitle_text),
+        "sort_title": store_entry(SeriesSortTitle_text),
+        "long": store_entry(SeriesLong_text),
+        "medium": store_entry(SeriesMedium_text),
+        "short": store_entry(SeriesShort_text),
+        "xshort": store_entry(SeriesXShort_text),
+        "tiny": store_entry(SeriesTiny_text),
+        "seo_keywords": store_entry(SeriesSEO_text),
+        "expected_duration": store_entry(seriesTitle_text),
+    }
+    write_to_csv(row)
+    print("Saved Series Form")
+
+def write_to_csv(dictionary):
+    with CSV_FILE.open("a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for key, value in dictionary.items():
+            if key == "seo_keywords":
+                keywords = split_seo_keywords(value)
+                # Write up the key, and then as separate column entries, each keyword.
+                writer.writerow([key] + keywords)
+            else:
+                # Write up the key and then the value in the next column.
+                writer.writerow([key, value])
+        # Writing an empty row to account for multiple saves.
+        writer.writerow([])
+
+def add_save_button(tab_frame, row, save_cmd):
+    btn = ttk.Button(tab_frame, text="Save Form", command=save_cmd)
+    btn.grid(column=1, row=row, sticky="e", pady=(10, 0))
+    return btn
+
 #Establishing the TTK skeleton
 root = Tk()
 root.title("Dalet Series/Season Form to CSV")
@@ -26,8 +74,8 @@ formFiller.pack(expand=True, fill="both")
 
 #Series Form
 ttk.Label(seriesView, text="Title").grid(column=0, row=0, sticky="w")
-SeriesTitle_text = Text(seriesView, height=2, width=60)
-SeriesTitle_text.grid(column=1, row=0, sticky="nsew")
+SeriesTitle_Titletext = Text(seriesView, height=2, width=60)
+SeriesTitle_Titletext.grid(column=1, row=0, sticky="nsew")
 
 ttk.Label(seriesView, text="Short Title (25 chars)").grid(column=0, row=1, sticky="w")
 SeriesShortTitle_text = Text(seriesView, height=2, width=60, wrap="word")
@@ -62,8 +110,10 @@ SeriesSEO_text = Text(seriesView, width=60, height=2, wrap="word")
 SeriesSEO_text.grid(column=1, row=8)
 
 ttk.Label(seriesView, text="Expected Duration").grid(column=0, row=9, sticky="w")
-seriesTitle_text = Text(seriesView, height=2, width=60)
-seriesTitle_text.grid(column=1, row=9, sticky="nsew")
+seriesTitle_Durationtext = Text(seriesView, height=2, width=60)
+seriesTitle_Durationtext.grid(column=1, row=9, sticky="nsew")
+seriesTitle_Durationtext.insert("1.0", "hh:mm:ss")
+add_save_button(seriesView, 10, store_series_form)
 
 #Season Form
 ttk.Label(seasonView, text="Title/Number").grid(column=0, row=0, sticky="w")
@@ -93,6 +143,7 @@ SeasonTiny_text.grid(column=1, row=5)
 ttk.Label(seasonView, text="SEO Keywords").grid(column=0, row=6, sticky="w")
 SeasonSEO_text = Text(seasonView, width=60, height=2, wrap="word")
 SeasonSEO_text.grid(column=1, row=6)
+
 
 #Episode Form
 ttk.Label(episodeView, text="Title").grid(column=0, row=0, sticky="w")
